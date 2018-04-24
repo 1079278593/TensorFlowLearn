c28d1ee1de03a90e9e693a6aaf1cf6b95b94af9d
ming <1079278593@qq.com>
2018-04-24 16:10:56 +0800

    1.增加猫狗识别；2.修改文件名字

---
+++ myLearning/03_dogCat_dataset.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 16:08:01 2018
+
+@author: ming
+"""
+
---
+++ myLearning/03_dogCat_predict.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 16:04:45 2018
+
+@author: ming
+"""
+
