212537ef79bb1fb42adde195a1e5f93281ebda65
ming <1079278593@qq.com>
2018-05-11 14:00:02 +0800

    猫狗识别代码和图片资源

--- myLearning/03_dogCat_dataset.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 24 16:08:01 2018
-
-@author: ming
-"""
-
--- myLearning/03_dogCat_train.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 24 16:08:30 2018
-
-@author: ming
-"""
-
